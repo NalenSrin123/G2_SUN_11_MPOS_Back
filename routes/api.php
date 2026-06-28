e8d0278953b5cd5a23867285cde9b7d46296773b
NalenSrin123 <srinnalen@gmail.com>
2026-06-28 16:04:00 +0700

    change from web to api

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,21 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('categories', CategoryController::class)->names('category');
     Route::apiResource('products', ProductController::class)->names('product');
 });
+use App\Http\Controllers\TableController;
+use  App\Http\Controllers\OrderController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/tables',         [TableController::class, 'index']);
+Route::get('/tables/{id}',    [TableController::class, 'show']);
+Route::post('/tables',        [TableController::class, 'store']);
+Route::put('/tables/{id}',    [TableController::class, 'update']);
+Route::delete('/tables/{id}', [TableController::class, 'destroy']);
+
+
+Route::get('/orders',         [OrderController::class, 'index']);
+Route::get('/orders/{id}',    [OrderController::class, 'show']);
+Route::post('/orders',        [OrderController::class, 'store']);
+Route::put('/orders/{id}',    [OrderController::class, 'update']);
+Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
