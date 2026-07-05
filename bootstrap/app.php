a76585e43d2e004bb3ac8be6bceed17c1642458a
Mongkol7 <thoeungsereymongkol@gmail.com>
2026-07-05 13:53:25 +0700

    Add Telegram error reporter to exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\TelegramErrorReporter;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,5 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->report(function (\Throwable $exception) {
+            app(TelegramErrorReporter::class)->report(
+                $exception,
+                app()->bound('request') ? request() : null
+            );
+        });
     })->create();
